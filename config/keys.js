ae4304a8d06198d8f618891174fed24e4f136335
Shinya Takahashi <shinyatkhs@gmail.com>
2018-05-13 14:10:51 +0900

    added user and profile router

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://shinya:shinY%401022@ds119490.mlab.com:19490/stoneshower_app'
+  mongoURI: 'mongodb://shinya:shinY%401022@ds119490.mlab.com:19490/stoneshower_app',
+  secretOrKey: 'secret'
 };
